a7eb99ffc7d1fbec47f461a9a639a727fc2afcaa
steve2030 <stevemighty2030@gmail.com>
2023-11-02 11:56:42 +0300

    modified the entity class

--- src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
@@ -1,4 +1,9 @@
 package com.eclectics.farmEasepro.equipments;
 
 public class Equipment {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int availability;
 }
