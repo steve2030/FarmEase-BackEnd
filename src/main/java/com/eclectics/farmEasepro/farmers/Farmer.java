711382f06808f95fe80b2bb69e69ba7cb33bc5fe
steve2030 <stevemighty2030@gmail.com>
2023-11-02 11:25:09 +0300

    added the constructor class

--- src/main/java/com/eclectics/farmEasepro/farmers/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/farmers/Farmer.java
@@ -14,10 +14,16 @@ public class Farmer {
     private int  phoneNumber;
 
 
+//    constructor class
 
 
-
-
+    public Farmer(Long id, String firstName, String lastName, String email, int phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
 
 
