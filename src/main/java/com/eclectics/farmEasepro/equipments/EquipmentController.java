f7f1f843f99e9520b3dd313ddc6768b3101074b5
steve2030 <stevemighty2030@gmail.com>
2023-11-02 19:00:54 +0300

    modified the equipment controller

--- src/main/java/com/eclectics/farmEasepro/equipments/EquipmentController.java
+++ src/main/java/com/eclectics/farmEasepro/equipments/EquipmentController.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.PublicKey;
+
+import java.util.List;
 
 @RestController
 public class EquipmentController {
     @Autowired
     private EquipmentService equipmentService;
     @GetMapping(path = "api/v1/equipments")
-    Public List<Equipment>getEquipments(){
+    public List<Equipment>getEquipments(){
         return equipmentService.getEquipments();
     }
 }
