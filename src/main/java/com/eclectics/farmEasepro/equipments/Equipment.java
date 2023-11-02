a5f67f8c460f168d4741291ab1665485b321641b
steve2030 <stevemighty2030@gmail.com>
2023-11-02 11:58:04 +0300

    added constructor class

--- src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
@@ -6,4 +6,15 @@ public class Equipment {
     private String description;
     private double price;
     private int availability;
+
+//    constructor
+
+
+    public Equipment(Long id, String name, String description, double price, int availability) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.availability = availability;
+    }
 }
