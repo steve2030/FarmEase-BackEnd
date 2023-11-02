ac5332ae40d1b9e3ebfb0e83aa579d21d8175208
steve2030 <stevemighty2030@gmail.com>
2023-11-02 11:59:44 +0300

    Generated getters and setters for the entity class

--- src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/equipments/Equipment.java
@@ -17,4 +17,46 @@ public class Equipment {
         this.price = price;
         this.availability = availability;
     }
+
+//    getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
 }
