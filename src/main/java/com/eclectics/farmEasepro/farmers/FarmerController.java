5920e1be680b66fc78071a7ad1c0b2c27aff0fa4
steve2030 <stevemighty2030@gmail.com>
2023-11-02 17:35:01 +0300

    modified the controller class

--- src/main/java/com/eclectics/farmEasepro/farmers/FarmerController.java
+++ src/main/java/com/eclectics/farmEasepro/farmers/FarmerController.java
@@ -3,8 +3,13 @@ package com.eclectics.farmEasepro.farmers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class FarmerController {
-    @GetMapping()
-
+   private FarmerService farmerService;
+    @GetMapping(path = "/api/v1/farmers")
+    public List<Farmer> getFarmers(){
+        return farmerService.getFarmers()
+    }
 }
